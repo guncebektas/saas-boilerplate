bf08b0c98fd303b2422f1707f711b9c2004a347c
gunce <guncebektas@gmail.com>
2024-03-21 13:18:35 +0300

    feat: many improvements

--- imports/modules/dummy/dummyService.js
+++ imports/modules/dummy/dummyService.js
@@ -1,21 +1,22 @@
 import {BaseService} from "../shared/service/baseService.js";
 import {DUMMY_LINKS} from "./enums/links";
-import {linkInsert} from "../link/link.methods";
 import {linkRepository} from "../link/linkRepository";
 import {linkService} from "../link/linkService";
 
-class DummyService extends BaseService {
-  constructor() {
+export class DummyService extends BaseService {
+  constructor(repository, service, data) {
     super();
+
+    this.repository = linkRepository;
+    this.service = linkService;
+    this.data = DUMMY_LINKS;
   }
 
-  async insertLinks() {
-    if (await linkRepository.find().countAsync() === 0) {
-      for (const link of DUMMY_LINKS) {
-        await linkService.add(link.title, link.url);
+  async insert() {
+    if (await this.repository.find().countAsync() === 0) {
+      for (const link of this.data) {
+        await this.service.add(link.title, link.url);
       }
     }
   }
 }
-
-export const dummyService = new DummyService();
--- server/main.js
+++ server/main.js
@@ -1,7 +1,11 @@
 import './init.js';
 import './modules.js';
-import {dummyService} from "../imports/modules/dummy/dummyService";
+import {DummyService} from "../imports/modules/dummy/dummyService";
+import {linkRepository} from "../imports/modules/link/linkRepository";
+import {linkService} from "../imports/modules/link/linkService";
+import {DUMMY_LINKS} from "../imports/modules/dummy/enums/links";
 
 Meteor.startup(async () => {
-  await dummyService.insertLinks();
+  const dummyService = new DummyService(linkRepository, linkService, DUMMY_LINKS)
+  await dummyService.insert();
 });
