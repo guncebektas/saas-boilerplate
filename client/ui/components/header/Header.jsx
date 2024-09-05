47bf79d0f7762f748a468fb7b0d7e05c6e3d18cc
gunce <guncebektas@gmail.com>
2024-09-05 22:32:35 +0300

    feat: onlyIcon prop added

--- client/ui/components/header/Header.jsx
+++ client/ui/components/header/Header.jsx
@@ -75,7 +75,10 @@ export const Header = () => {
           <HeaderNotifications/>
           <HeaderApps/>
           <HeaderProfile/>
-          <LanguageSelector/>
+
+          <div className="p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
+            <LanguageSelector/>
+          </div>
         </div>
       </div>
     </nav>
--- client/ui/components/languageSelector/LanguageSelector.jsx
+++ client/ui/components/languageSelector/LanguageSelector.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import {Dropdown} from "flowbite-react";
 import {useTranslator} from "../../providers/i18n";
 import {onChangeLocale} from "../../../shared/functions/onChangeLocale";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faLanguage } from '@fortawesome/free-solid-svg-icons'; // Import the language icon
 
-export const LanguageSelector = () => {
+export const LanguageSelector = (onlyIcon = false) => {
   const t = useTranslator();
   const {supportedLanguages} = Meteor.settings.public.app;
 
+  const label = onlyIcon ? (
+    <FontAwesomeIcon icon={faLanguage}/>
+  ) : (
+    t("Language")
+  );
+
   return (
-    <Dropdown label={t('Language')} inline className="mr-3">
+    <Dropdown label={label} inline className="mr-3">
       {supportedLanguages.map(({ languageCode, languageLabel }) => (
         <Dropdown.Item key={languageCode} onClick={() => onChangeLocale(languageCode)}>
           <img src={`online/images/flags/${languageCode}.svg`} alt={languageLabel} className="w-5 mr-1"/>
