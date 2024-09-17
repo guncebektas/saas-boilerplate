3813cec9cb0a2e750196f1a17b927ce5e8975bce
gunce <guncebektas@gmail.com>
2024-09-18 00:44:49 +0300

    feat: minor updates

--- imports/modules/users/server/user.onCreate.js
+++ imports/modules/users/server/user.onCreate.js
@@ -2,14 +2,16 @@ import {Accounts} from 'meteor/accounts-base';
 import {event} from "../../shared/event/server/init.js";
 import {EVENT} from "../../shared/enums/event.js";
 
-const _isFirstUser = () => {
-  return Meteor.users.find().count() === 1;
+const _isFirstUser = async () => {
+  return await Meteor.users.find().countAsync() === 1;
 };
 
-Accounts.onCreateUser(function (options, user) {
+Accounts.onCreateUser(async function (options, user) {
   // this is our first user. Thus, we are adding all roles
-  if (_isFirstUser) {
+  if (await _isFirstUser()) {
     event.emit(EVENT.SET_ADMIN_ROLE, {userId: user._id});
+  } else {
+    event.emit(EVENT.SET_USER_ROLE, {userId: user._id});
   }
 
   event.emit(EVENT.SET_PROFILE, {userId: user._id});
