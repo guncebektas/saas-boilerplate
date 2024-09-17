96ffb6704087f4e1c10be2ac4dd94279257f1b45
gunce <guncebektas@gmail.com>
2024-09-18 00:42:58 +0300

    add: EVENT.SET_USER_ROLE

--- imports/modules/userRoles/server/event.js
+++ imports/modules/userRoles/server/event.js
@@ -2,7 +2,14 @@ import {event} from "../../shared/event/server/init.js";
 import {EVENT} from "../../shared/enums/event.js";
 import {ROLE} from "../../shared/enums/role.js";
 import {Roles} from "meteor/alanning:roles";
+import {ROLE_SCOPE} from "../../shared/enums/roleScope";
 
 event.on(EVENT.SET_ADMIN_ROLE, async function ({userId}) {
-  await Roles.addUsersToRolesAsync(userId, Object.values(ROLE));
+  await Roles.addUsersToRolesAsync(userId, Object.values(ROLE), ROLE_SCOPE.USER);
 });
+
+event.on(EVENT.SET_USER_ROLE, async function ({userId}) {
+  await Roles.addUsersToRolesAsync(userId, ROLE.USER, ROLE_SCOPE.USER);
+});
+
+
