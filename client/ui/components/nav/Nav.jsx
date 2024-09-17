c96e89364292a061c7e5220f6df9964a60b26ac8
gunce <guncebektas@gmail.com>
2024-09-18 00:43:47 +0300

    add: NavFooter become a component

--- client/ui/components/nav/Nav.jsx
+++ client/ui/components/nav/Nav.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {NavItem} from "./NavItem.jsx";
 import {ROUTE} from "../../../routes/enums/route.js";
-import {Sidebar, Tooltip} from 'flowbite-react';
+import {Sidebar} from 'flowbite-react';
 import {HiOutlineMinusSm, HiOutlinePlusSm, HiShoppingBag} from "react-icons/hi";
 import {twMerge} from "tailwind-merge";
+import {NavFooter} from "./NavFooter";
 
 export const Nav = () => {
   return (
@@ -58,24 +57,7 @@ export const Nav = () => {
           </li>
         </ul>
       </div>
-      <div className="absolute bottom-0 left-0 w-full flex justify-center p-4 space-x-4 bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700">
-        <Link to={ROUTE.ADMIN} data-tooltip-target="tooltip-admin" className="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600">
-          <FontAwesomeIcon icon="user-tie"/>
-        </Link>
-        <div id="tooltip-admin" role="tooltip" className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip">
-          Admin
-          <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
 
-        <Link to={ROUTE.SETTINGS} data-tooltip-target="tooltip-settings" className="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600">
-          <FontAwesomeIcon icon="cog"/>
-        </Link>
-        <div id="tooltip-settings" role="tooltip" className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip">
-          Settings
-          <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
-      </div>
+      <NavFooter/>
     </Sidebar>
-)
-  ;
-};
+)};
