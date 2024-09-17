c03022c0eb7f8310bfb20ef3fc2996af20502046
gunce <guncebektas@gmail.com>
2024-09-18 00:42:40 +0300

    add: isFeatureEnabled

--- imports/modules/organizations/organizationService.js
+++ imports/modules/organizations/organizationService.js
@@ -1,15 +1,14 @@
 import {BaseService} from "../shared/service/baseService";
 import {organizationRepository} from "./organizationRepository";
+import {isOrganizationEnabled} from "../shared/functions/isOrganizationEnabled";
 
 class OrganizationService extends BaseService{
-  constructor(repository) {
-    super();
-
-    this.repository = repository
+  constructor({repository, isFeatureEnabled}) {
+    super({ repository, isFeatureEnabled });
   }
 
   async create(userId) {
-    await this.repository.insertAsync({_id: userId});
+   await this.repository.insertAsync({_id: userId});
   }
 
   async addKey() {
@@ -21,5 +20,8 @@ class OrganizationService extends BaseService{
   }
 }
 
-export const organizationService = new OrganizationService(organizationRepository)
+export const organizationService = new OrganizationService({
+  repository: organizationRepository,
+  isFeatureEnabled: isOrganizationEnabled,
+})
 
--- imports/modules/shared/service/baseService.js
+++ imports/modules/shared/service/baseService.js
@@ -1,6 +1,31 @@
 export class BaseService {
-  constructor(repository) {
+  constructor({repository, isFeatureEnabled}) {
     this.repository = repository;
+
+    // Default to always enabling if no feature check is provided
+    this.isFeatureEnabled = isFeatureEnabled || (() => true);
+
+    // Automatically wrap all methods with the feature check
+    this.wrapMethods();
+  }
+
+  wrapMethods() {
+    const methodNames = Object
+      .getOwnPropertyNames(Object.getPrototypeOf(this))
+      .filter(methodName => typeof this[methodName] === 'function' && methodName !== 'constructor');
+
+    methodNames.forEach(methodName => {
+      const originalMethod = this[methodName];
+
+      // Override the method to include feature check
+      this[methodName] = async (...args) => {
+        if (!this.isFeatureEnabled()) {
+          return; // Skip execution if feature is disabled
+        }
+
+        return originalMethod.apply(this, args); // Execute the original method
+      };
+    });
   }
 
   /**
