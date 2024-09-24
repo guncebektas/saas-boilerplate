e05eabad4b44c138adaea1a1acf7a2c6c27139bf
gunce <guncebektas@gmail.com>
2024-09-24 21:40:20 +0300

    fix: otp store to db

--- client/ui/components/modals/QRCodeModal.jsx
+++ client/ui/components/modals/QRCodeModal.jsx
@@ -3,20 +3,22 @@ import { Button, Modal } from 'flowbite-react';
 import { QRCodeCanvas } from 'qrcode.react';
 import { Meteor } from 'meteor/meteor';
 import Countdown from "../countDown/CountDown";
+import {profileSaveOtp} from "../../../../imports/modules/userProfiles/userProfile.methods";
 
 export const QRCodeModal = ({ isOpen, onClose }) => {
   const [otp, setOtp] = useState(generateOTP());
+  profileSaveOtp({otp});
 
-  // Function to generate a new OTP
   function generateOTP() {
     return Math.floor(100000 + Math.random() * 900000).toString();
   }
 
   const handleExpire = () => {
     setOtp(generateOTP()); // Generate a new OTP when the countdown expires
+    profileSaveOtp({otp});
   };
 
-  const { logo } = Meteor.settings.public.app;
+  const {logo} = Meteor.settings.public.app;
 
   return (
     <Modal show={isOpen} size="md" onClose={onClose}>
@@ -37,7 +39,7 @@ export const QRCodeModal = ({ isOpen, onClose }) => {
         <div className="flex justify-center">
           <p className="scale-150 font-extrabold text-center py-3">{otp}</p>
         </div>
-        <Countdown initialSeconds={60} onExpire={handleExpire} />
+        <Countdown initialSeconds={60} onExpire={handleExpire}/>
       </Modal.Body>
       <Modal.Footer>
         <Button color="gray" onClick={onClose}>
