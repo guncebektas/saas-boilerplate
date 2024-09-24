50b1d7ecc2c89ad299108da87975c255d46ada20
gunce <guncebektas@gmail.com>
2024-09-24 21:57:48 +0300

    add: title class

--- client/ui/components/heading/Headings.jsx
+++ client/ui/components/heading/Headings.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 export const H2 = ({text}) => {
-  return (<h2 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white mb-3">{text}</h2>);
+  return (<h2 className="title text-4xl mb-3">{text}</h2>);
 };
 
 export const H3 = ({text}) => {
-  return (<h3 className="text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white mb-3">{text}</h3>);
+  return (<h3 className="title text-2xl mb-3">{text}</h3>);
 };
 
 export const H4 = ({text}) => {
-  return (<h4 className="tracking-tight font-extrabold text-gray-900 dark:text-white mb-3">{text}</h4>);
+  return (<h4 className="title mb-3">{text}</h4>);
 };
