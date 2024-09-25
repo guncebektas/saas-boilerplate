2a1b1680579a565a8481819915089f9b3a5e28a2
gunce <guncebektas@gmail.com>
2024-09-25 22:16:36 +0300

    add: logo

--- client/ui/components/credits/Credits.jsx
+++ client/ui/components/credits/Credits.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+
 export const Credits = () => {
   return (
     <section className="fixed block w-full bottom-0 bg-white px-3 py-3 dark:bg-gray-900">
-      <p className="text-gray-500 text-right">Powered by <a href="https://ritapos.com" target="_blank">Ritapos</a></p>
+      <p className="text-gray-500 text-right text-xs">
+        <span>Powered by </span>
+        <a
+          href="https://ritapos.com"
+          target="_blank"
+          className="float-right align-bottom"
+        >
+          <img src="https://app.ritapos.com/logo.png" alt="Ritapos" width="38" className="float-right"/>
+        </a>
+      </p>
     </section>
   );
 };
