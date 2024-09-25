64463385d5790af761deb141f67f73cbdb7dd454
gunce <guncebektas@gmail.com>
2024-09-25 14:32:40 +0300

    rem: redundant files collection

--- imports/modules/shared/database/filesCollection.js
+++
@@ -1,7 +0,0 @@
-import { FilesCollection } from 'meteor/ostrio:files';
-
-export const Files = new FilesCollection({
-  collectionName: 'Files',
-  allowClientCode: false, // Disallow remove files from the client
-  // storagePath: '/path/to/storage', // Define where to store files
-});
