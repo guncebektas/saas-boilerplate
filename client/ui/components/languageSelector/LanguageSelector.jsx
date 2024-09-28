1115018b044246fb8735a52b32c6dc776f91161b
gunce <guncebektas@gmail.com>
2024-09-28 23:23:39 +0300

    add: show flag as icon

--- client/ui/components/languageSelector/LanguageSelector.jsx
+++ client/ui/components/languageSelector/LanguageSelector.jsx
@@ -2,27 +2,20 @@ import React from "react";
 import {Dropdown} from "flowbite-react";
 import {useTranslator} from "../../providers/i18n";
 import {onChangeLocale} from "../../../../imports/modules/shared/functions/onChangeLocale";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faLanguage } from '@fortawesome/free-solid-svg-icons';
+import i18n from "meteor/universe:i18n";
 
 export const LanguageSelector = (onlyIcon = false) => {
   const t = useTranslator();
+
+  const currentLanguage = i18n.getLocale();
   const {supportedLanguages} = Meteor.settings.public.app;
 
-  const label = onlyIcon ? (
-    <FontAwesomeIcon icon={faLanguage}/>
-  ) : (
-    t("Language")
-  );
+  const label = onlyIcon ? (<img src={`/online/images/flags/${currentLanguage}.svg`} alt={t("Language")} className="w-5 mr-1"/>) : (t("Language"));
 
-  return (
-    <Dropdown label={label} inline className="mr-3">
-      {supportedLanguages.map(({ languageCode, languageLabel }) => (
-        <Dropdown.Item key={languageCode} onClick={() => onChangeLocale(languageCode)}>
+  return (<Dropdown label={label} inline className="mr-3">
+      {supportedLanguages.map(({languageCode, languageLabel}) => (<Dropdown.Item key={languageCode} onClick={() => onChangeLocale(languageCode)}>
           <img src={`/online/images/flags/${languageCode}.svg`} alt={languageLabel} className="w-5 mr-1"/>
           {languageLabel}
-        </Dropdown.Item>
-      ))}
-    </Dropdown>
-  );
+        </Dropdown.Item>))}
+    </Dropdown>);
 };
