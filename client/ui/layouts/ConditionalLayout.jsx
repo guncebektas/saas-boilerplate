92115063eb018b38059128364cb34b838d18be74
gunce <guncebektas@gmail.com>
2024-09-28 23:23:58 +0300

    fix: icon usage

--- client/ui/layouts/ConditionalLayout.jsx
+++ client/ui/layouts/ConditionalLayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Header} from "../components/header/Header.jsx";
 import {Nav} from "../components/nav/Nav.jsx";
 import {Router} from "../../routes/Router.js";
@@ -14,23 +14,22 @@ import {useTranslator} from "../providers/i18n";
 const InnerLayout = () => {
   const t = useTranslator();
 
-  const {name, logo} = Meteor.settings.public.app;
+  const {name, logo, icon} = Meteor.settings.public.app;
 
   const user = useTracker(() => { return Meteor.userId() });
   if (user) {
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+    const handleToggleSidebar = () => {
+      setSidebarOpen(!isSidebarOpen);
+    };
+
     return (
       <section className="dark:bg-gray-800">
-        <button data-drawer-target="drawer-navigation" data-drawer-toggle="drawer-navigation" aria-controls="drawer-navigation" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-          <span className="sr-only">{t('Open sidebar')}</span>
-          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-          </svg>
-        </button>
+        <Header onToggleSidebar={handleToggleSidebar}/>
+        <Nav isOpen={isSidebarOpen}/>
 
-        <Header/>
-        <Nav/>
-
-        <main className="px-4 md:ml-64 h-auto sm:pt-20">
+        <main className="px-4 md:ml-64 h-auto pt-20">
           <section className="bg-white px-4 py-5 sm:p-6 dark:bg-gray-900">
             <Router/>
           </section>
@@ -47,7 +46,7 @@ const InnerLayout = () => {
     <section className="dark:bg-gray-900">
       <Navbar rounded className="mb-3">
         <Navbar.Brand as={Link} href="/">
-          <img src={logo} alt={name}/>
+          <img src={icon} alt={name} className="mr-3"/>
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">{name}</span>
         </Navbar.Brand>
         <Navbar.Toggle/>
