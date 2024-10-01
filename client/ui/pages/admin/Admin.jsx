a54b3bdc8e79efa8c97f39e4be52ede16b80645e
gunce <guncebektas@gmail.com>
2024-10-01 11:04:40 +0300

    add: user profile link

--- client/ui/pages/admin/Admin.jsx
+++ client/ui/pages/admin/Admin.jsx
@@ -4,8 +4,6 @@ import {ROUTE} from "../../../routes/enums/route";
 import {Button} from "flowbite-react";
 import {useTranslator} from "../../providers/i18n";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
-import {NavbarLink} from "flowbite-react/lib/esm/components/Navbar/NavbarLink";
 import {FaEnvelope} from "react-icons/fa";
 import {useNavigate} from "react-router-dom";
 
@@ -23,8 +21,16 @@ export const Admin = () => {
 
       <div className="my-6">
         <Button
+          onClick={() => handleNavigate(ROUTE.USER_PROFILES)}
+          color="blue"
+          className="w-full flex justify-start items-center mb-3"
+        >
+          <FontAwesomeIcon icon="user" className="text-xl mr-1"/>
+          <span>{t('User profiles')}</span>
+        </Button>
+        <Button
           onClick={() => handleNavigate(ROUTE.CONTACT_REQUESTS)}
-          color="success"
+          color="blue"
           className="w-full flex justify-start items-center"
         >
           <FaEnvelope className="text-xl mr-1"/>
