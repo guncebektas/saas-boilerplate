a6132b3f62332c82c29716ae75dc0138558a0c41
gunce <guncebektas@gmail.com>
2024-10-11 20:53:41 +0300

    rem: click increase

--- client/ui/pages/Hello.jsx
+++ client/ui/pages/Hello.jsx
@@ -12,31 +12,16 @@ export const Hello = () => {
 
   const welcomeMessage = `${t('Welcome to {$name}', {name: name})}!`
 
-  const [counter, setCounter] = useState(0);
-
-  const increment = () => {
-    setCounter(counter + 1);
-  };
-
   return (
     <>
-      <div className="px-4 py-5 sm:p-6 sm:pt-8">
+      <div className="py-5 sm:pt-8">
         <div className="sm:flex sm:items-start sm:justify-between">
           <div>
             <div className="flex items-center">
               <H2 text={welcomeMessage}/>
             </div>
-            <div className="mt-2 max-w-xl text-gray-500 text-lg">
-              <p>{t('You\'ve pressed the button {$counter} times', {counter: counter})}.</p>
-            </div>
           </div>
           <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
-            <Button
-              gradientMonochrome="purple"
-              onClick={increment}
-            >
-              {t('Click')}
-            </Button>
           </div>
         </div>
       </div>
