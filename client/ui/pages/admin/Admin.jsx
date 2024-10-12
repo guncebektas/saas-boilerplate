ea70674a26f95c9d965c97e04095fea9a90f47d2
gunce <guncebektas@gmail.com>
2024-10-12 08:34:43 +0300

    feat: same style implemented

--- client/ui/pages/admin/Admin.jsx
+++ client/ui/pages/admin/Admin.jsx
@@ -1,42 +1,46 @@
 import React from 'react';
-import {H2} from "../../components/heading/Headings.jsx";
-import {ROUTE} from "../../../routes/enums/route";
-import {Button} from "flowbite-react";
-import {useTranslator} from "../../providers/i18n";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {FaEnvelope} from "react-icons/fa";
-import {useNavigate} from "react-router-dom";
+import { H2 } from "../../components/heading/Headings";
+import { useTranslator } from "../../providers/i18n";
+import { Button } from "flowbite-react";
+import { FaUser, FaEnvelope } from 'react-icons/fa';
+import { ROUTE } from "../../../routes/enums/route";
+import { useNavigate } from "react-router-dom";
 
 export const Admin = () => {
   const t = useTranslator();
   const navigate = useNavigate();
 
-  const handleNavigate = (route) => {
-    navigate(route);
-  };
+  const buttons = [
+    {
+      icon: <FaUser className="text-xl mr-1" />,
+      text: t('User profiles'),
+      color: 'blue',
+      onClick: () => navigate(ROUTE.USER_PROFILES),
+    },
+    {
+      icon: <FaEnvelope className="text-xl mr-1" />,
+      text: t('Contact requests'),
+      color: 'blue',
+      onClick: () => navigate(ROUTE.CONTACT_REQUESTS),
+    }
+  ];
 
   return (
     <>
-      <H2 text="Admin"/>
+      <H2 text={t('Admin')} />
 
-      <div className="my-6">
-        <Button
-          onClick={() => handleNavigate(ROUTE.USER_PROFILES)}
-          color="blue"
-          className="w-full flex justify-start items-center mb-3"
-        >
-          <FontAwesomeIcon icon="user" className="text-xl mr-1"/>
-          <span>{t('User profiles')}</span>
-        </Button>
-        <Button
-          onClick={() => handleNavigate(ROUTE.CONTACT_REQUESTS)}
-          color="blue"
-          className="w-full flex justify-start items-center"
-        >
-          <FaEnvelope className="text-xl mr-1"/>
-          <span>{t('Contact requests')}</span>
-        </Button>
-      </div>
+      {buttons.map((button, index) => (
+        <div key={index} className="my-6">
+          <Button
+            onClick={button.onClick}
+            color={button.color}
+            className="w-full flex justify-start items-center mb-3"
+          >
+            {button.icon}
+            <span>{button.text}</span>
+          </Button>
+        </div>
+      ))}
     </>
   );
-}
+};
--- client/ui/pages/settings/Settings.jsx
+++ client/ui/pages/settings/Settings.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
-import {H2} from "../../components/heading/Headings.jsx";
+import React from "react";
+import {H2} from "../../components/heading/Headings";
+import {useTranslator} from "../../providers/i18n";
+import {Button} from 'flowbite-react';
+import {FaHome, FaStar, FaInfoCircle} from 'react-icons/fa';
+import {ROUTE} from "../../../routes/enums/route";
+import {useNavigate} from "react-router-dom";
 
 export const Settings = () => {
+  const t = useTranslator();
+  const navigate = useNavigate();
+
+  const buttons = [
+    {
+      icon: <FaHome className="text-xl mr-1"/>,
+      text: t('Home'),
+      color: 'blue',
+      onClick: () => navigate(ROUTE.FAQS),
+    },
+    {
+      icon: <FaStar className="text-xl mr-1"/>,
+      text: t('Wallet'),
+      color: 'blue',
+      onClick: () => navigate(ROUTE.CONTACT_FORM),
+    },
+    {
+      icon: <FaInfoCircle className="text-xl mr-1"/>,
+      text: t('About'),
+      color: 'blue',
+      onClick: () => navigate(ROUTE.CONTACT_FORM),
+    }
+  ];
+
   return (
     <>
-      <H2 text="Settings"></H2>
-      <div className="grid grid-flow-col justify-stretch space-x-4">
+      <H2 text={t('Settings')}/>
 
-      </div>
+      {buttons.map((button, index) => (
+        <div key={index} className="my-6">
+          <Button
+            onClick={button.onClick}
+            color={button.color}
+            gradientDuoTone={button.gradient || undefined}
+            className="w-full flex justify-start items-center mb-3"
+          >
+            {button.icon}
+            <span>{button.text}</span>
+          </Button>
+        </div>
+      ))}
     </>
   );
 };
