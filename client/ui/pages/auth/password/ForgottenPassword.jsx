45e9d095870519691a8c7d9c13f4c6cf28f3d5f8
gunce <guncebektas@gmail.com>
2024-10-16 22:36:46 +0300

    fix: localization

--- client/ui/pages/auth/password/ForgottenPassword.jsx
+++ client/ui/pages/auth/password/ForgottenPassword.jsx
@@ -1,49 +1,48 @@
 import React, {useRef} from 'react';
 import {Button, Label, TextInput} from 'flowbite-react';
 import {STATE_AUTH_PASSWORD_FORM} from "./enums/state.js";
-import {Log} from "meteor/logging";
+import {userSendResetPasswordEmail} from "../../../../../imports/modules/users/user.methods";
+import {useTranslator} from "../../../providers/i18n";
 
 export const ForgottenPassword = ({onStateChange}) => {
+  const t = useTranslator();
+
   const emailRef = useRef();
 
   const handleState = () => {
     onStateChange(STATE_AUTH_PASSWORD_FORM.LOGIN);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const formData = {
       email: emailRef.current.value,
     };
 
-    Accounts.forgotPassword({
-      email: formData.email
-    }, (error, response) => {
-      Log.error(error);
-    });
+    await userSendResetPasswordEmail({email: formData.email})
   };
 
   return (
     <div className="sm:mx-auto sm:w-full sm:max-w-md">
-      <h2 className="text-center text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl">Forgotten Password</h2>
+      <h2 className="text-center text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl">{t('Forgotten password')}</h2>
 
       <div className="bg-white dark:bg-gray-900 py-8 px-4 mt-8 shadow sm:rounded-lg sm:px-10">
         <div>
           <form className="space-y-6" onSubmit={handleSubmit}>
             <div className="mb-2">
               <div className="mb-2 block">
-                <Label htmlFor="email" value="Email Address"/>
+                <Label htmlFor="email" value={t('Email address')}/>
               </div>
               <TextInput id="email" type="email" ref={emailRef} required/>
             </div>
             <div>
-              <Button type="submit" color="primary">Send</Button>
+              <Button type="submit" color="primary">{t('Submit')}</Button>
             </div>
 
             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-              Don’t have an account yet?
-              <button className="font-medium hover:underline mx-1" onClick={() => handleState(STATE_AUTH_PASSWORD_FORM.LOGIN)}>Register</button>
+              {t('Don’t have an account yet')}?
+              <button className="font-medium hover:underline mx-1" onClick={() => handleState(STATE_AUTH_PASSWORD_FORM.REGISTER)}>{t('Register')}</button>
             </p>
           </form>
         </div>
