d159284cc9fe14874b03fc03270db0dfe4d93502
gunce <guncebektas@gmail.com>
2024-10-22 23:00:44 +0300

    feat: file become userProfilePicture add: faqs

--- imports/modules/shared/enums/collectionNames.js
+++ imports/modules/shared/enums/collectionNames.js
@@ -1,9 +1,10 @@
 export const COLLECTION_NAME = {
   CONTACT_REQUESTS: 'contactRequests',
-  FILES: 'files',
+  FAQS: 'faqs',
   LINKS: 'links',
   NOTIFICATIONS: 'notifications',
   ORGANIZATIONS: 'organizations',
   USER_PROFILES: 'userProfiles',
+  USER_PROFILE_PICTURE: 'userProfilePicture',
   TICKETS: 'tickets'
 };
