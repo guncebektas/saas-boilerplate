bc802a622113b049377bce96e10ab8ca41a80d7e
gunce <guncebektas@gmail.com>
2024-10-28 14:33:04 +0300

    feat: check isAdminMixin before method execution

--- imports/modules/app/user/userRoles/userRolesMethods.js
+++ imports/modules/app/user/userRoles/userRolesMethods.js
@@ -1,13 +1,14 @@
 import {createMethod} from 'meteor/jam:method'; // can import { Methods } from 'meteor/jam:method' instead and use Methods.create if you prefer
 import {userRoleService} from "./userRoleService.js";
 import {oneRowSchema} from "../../../shared/schemas/oneRowSchema";
+import {isAdminMixin} from "../../../shared/mixins/isAdminMixin";
 
 export const userRolesMethods = {
   setAsManager: createMethod({
     name: 'userRoles.setAsManager',
     schema: oneRowSchema,
     serverOnly: true,
-    open: true,
+    before: isAdminMixin,
     async run({ _id }) {
       return userRoleService.setAsManager(_id);
     }
