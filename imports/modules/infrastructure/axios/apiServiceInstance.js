a45690120d82f8545d2e4453071898c4ba620d61
gunce <guncebektas@gmail.com>
2024-11-02 09:50:11 +0300

    feat: changes to extend in other services

--- imports/modules/infrastructure/axios/apiServiceInstance.js
+++ imports/modules/infrastructure/axios/apiServiceInstance.js
@@ -1,13 +1,24 @@
 import {AxiosExampleContract} from "./axiosExample.contract";
 import {ApiService} from "./apiService";
 
-// Define contract patterns
-const contractRegistry = [
-  { pattern: 'https://app.ritapos.com/api/v1/orders/*', contract: AxiosExampleContract },
-  { pattern: 'https://app.ritapos.com/api/v1/customers/*', contract: AxiosExampleContract },
-];
+export default class ApiServiceInstance extends ApiService {
+  constructor() {
+    const contractRegistry = [
+      { pattern: 'https://app.ritapos.com/api/v1/customers/*', contract: AxiosExampleContract },
+      { pattern: 'https://app.ritapos.com/api/v1/franchises/*', contract: AxiosExampleContract },
+      { pattern: 'https://app.ritapos.com/api/v1/orders/*', contract: AxiosExampleContract },
+      { pattern: 'https://app.ritapos.com/api/v1/stores/*', contract: AxiosExampleContract },
+    ];
 
-// Create an instance of ApiService with the pattern-based contract configuration
-const apiServiceInstance = new ApiService(contractRegistry);
+    super(contractRegistry);
 
-export default apiServiceInstance;
+    if (Meteor.isDevelopment) {
+      this.baseUrl = 'http://localhost:3000/';
+      // this.baseUrl = 'https://app.ritapos.com/';
+    } else {
+      this.baseUrl = 'https://app.ritapos.com/';
+    }
+  }
+}
+
+export const apiServiceInstance = new ApiService();
