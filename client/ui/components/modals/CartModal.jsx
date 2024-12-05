7c7e880021461592b0e1adeb8321570171d1ef47
gunce <guncebektas@gmail.com>
2024-12-05 20:15:23 +0300

    add: clear cart button

--- client/ui/components/modals/CartModal.jsx
+++ client/ui/components/modals/CartModal.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {Button, Modal} from 'flowbite-react';
 import {useTranslator} from '../../providers/i18n';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHeart, faMapMarkerAlt, faMinus, faPhone, faPlus} from '@fortawesome/free-solid-svg-icons';
+import {faCartShopping, faHeart, faMinus, faPlus} from '@fortawesome/free-solid-svg-icons';
 import {useCartStore} from '../../stores/useCartStore';
-import Map from "../map/Map";
 import CurrencyDisplay from "../currencyDisplay/currencyDiplay";
+import {Link} from "react-router-dom";
+import {ROUTE} from "../../../routes/enums/route";
 
 export const CartModal = () => {
   const t = useTranslator();
+
+  const productCount = useCartStore((state) => state.products.length);
 
   const isCartModalOpen = useCartStore((state) => state.isCartModalOpen);
   const closeCartModal = useCartStore((state) => state.closeCartModal);
@@ -16,30 +19,37 @@ export const CartModal = () => {
   const products = useCartStore((state) => state.products);
   const pushProduct = useCartStore((state) => state.pushProduct);
   const pullProduct = useCartStore((state) => state.pullProduct);
+  const clearCart = useCartStore((state) => state.clearCart);
 
   const handleCheckout = () => {
     console.log('handleCheckout')
   }
   return (
-    <Modal show={isCartModalOpen} onClose={closeCartModal} size="md">
-      <Modal.Header>{t('Your cart')}</Modal.Header>
+    <Modal show={isCartModalOpen} onClose={closeCartModal}>
+      <Modal.Header>
+        <FontAwesomeIcon icon={faCartShopping}/>
+        <span className={"relative"}>
+          {t('Your cart')}
+          <span className="absolute top-0 left-20 -translate-y-1/2 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+            {productCount}
+          </span>
+        </span>
+      </Modal.Header>
       <Modal.Body>
         <div className="flex flex-col space-y-4">
           {products && products.map((product) => (
             <div key={product.rowNumber} className="flex justify-between items-center p-4 border rounded-lg shadow-sm">
               <div>
-                <p className="text-xs text-gray-500">{product.categoryTitle}</p>
-                <h3 className="text-lg font-semibold">{product.title}</h3>
-                <p className="text-sm text-gray-500">
-                  <CurrencyDisplay price={product.priceOut} currency="TRY" locale="tr-TR"/>
-                </p>
+                <p className="text-xs m-text">{product.categoryTitle}</p>
+                <h3 className="text-lg font-semibold m-title">{product.title}</h3>
+                <CurrencyDisplay price={product.priceOut} currency="TRY" locale="tr-TR"/>
               </div>
               <Button.Group>
                 <Button
                   size="sm"
                   color="green"
                   className={"text-green-300 mr-1"}
-                  onClick={() => pushProduct(product)} // Define this action in your `useCartStore`
+                  onClick={() => pushProduct(product)}
                 >
                   <FontAwesomeIcon icon={faPlus}/>
                 </Button>
@@ -47,7 +57,7 @@ export const CartModal = () => {
                   size="sm"
                   color="red"
                   className={"text-red-300"}
-                  onClick={() => pullProduct(product)} // Define this action in your `useCartStore`
+                  onClick={() => pullProduct(product)}
                 >
                   <FontAwesomeIcon icon={faMinus}/>
                 </Button>
@@ -60,13 +70,17 @@ export const CartModal = () => {
         </div>
       </Modal.Body>
       <Modal.Footer>
+        <Button color="red" onClick={clearCart}>
+          {t('Clear')}
+        </Button>
         <div className="flex justify-between w-full">
           <Button color="default" onClick={closeCartModal}>
             {t('Close')}
           </Button>
-          <Button color="blue" onClick={handleCheckout}>
-            <FontAwesomeIcon icon={faHeart} className="mr-2"/> {t('Pay now')}
-          </Button>
+          <Link to={ROUTE.CHECKOUT} className="btn btn-blue flex items-center">
+            <FontAwesomeIcon icon={faHeart} className="mr-2" />
+            {t('Pay now')}
+          </Link>
         </div>
       </Modal.Footer>
     </Modal>
