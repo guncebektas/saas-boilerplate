9c1dcca91d3c180f53b16d5bbe95cb039a200995
gunce <guncebektas@gmail.com>
2024-12-05 20:15:35 +0300

    rem: global app store

--- client/ui/stores/useAppStore.jsx
+++
@@ -1,9 +0,0 @@
-import {create} from 'zustand';
-
-export const useAppStore = create((set) => ({
-  showSidebarMenu: false,
-  toggleSidebarMenu: () => {
-    alert(!this.showSidebarMenu)
-    set({showSidebarMenu: !this.showSidebarMenu})
-  }
-}));
