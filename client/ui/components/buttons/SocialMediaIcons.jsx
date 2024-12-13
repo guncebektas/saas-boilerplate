2b1c6266ebaf44bd853425f83d25dec24e4f0594
gunce <guncebektas@gmail.com>
2024-12-13 21:51:36 +0300

    add: url checker

--- client/ui/components/buttons/SocialMediaIcons.jsx
+++ client/ui/components/buttons/SocialMediaIcons.jsx
@@ -4,7 +4,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 export const SocialMediaIcons = ({links}) => {
   return (
     <div className="flex space-x-4 mt-4">
-      {links.map(({ icon, url, alt }, index) => (
+      {links.filter(({ url }) => url).map(({ icon, url, alt }, index) => (
         <a key={index} href={url} target="_blank" rel="noopener noreferrer" aria-label={alt}>
           <FontAwesomeIcon icon={icon} className="text-gray-500 hover:text-gray-800 transition-colors duration-300" size="lg" />
         </a>
