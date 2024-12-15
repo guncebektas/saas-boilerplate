cfc3e7eb1774ecc26f45a84738f70be12fd114e0
gunce <guncebektas@gmail.com>
2024-12-15 19:38:20 +0300

    fix: only show menu on bigger screens

--- client/ui/stores/useAppStore.jsx
+++ client/ui/stores/useAppStore.jsx
@@ -1,7 +1,7 @@
 import {create} from 'zustand';
 
 export const useAppStore = create((set) => ({
-  isSidebarOpen: true,
+  isSidebarOpen: window.innerWidth > 768,
   openSidebar: () => set({ isSidebarOpen: true}),
   closeSidebar: () => set({ isSidebarOpen: false }),
 }));
